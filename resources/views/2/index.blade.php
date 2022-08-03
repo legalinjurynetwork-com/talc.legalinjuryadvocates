042a3d6cd316bfbfb521fda45618fb43a493a381
Sayan Saha <sayansaha08.02.90@gmail.com>
2022-08-04 00:36:24 +0530

    updated fields

--- resources/views/2/index.blade.php
+++ resources/views/2/index.blade.php
@@ -1,33 +1,34 @@
+@php
+$phone = "(800) 484-6757";
+$tel = "8004846757";
+$year = date("Y");
+$url = url('');
+$logo = "";
+$company = "";
+$style = "<style></style>";
+@endphp
+<?php
+if(str_contains($url, 'consumer')){
+    $company = "Consumer Injury Advocate";
+    $logo = "img/lin-logo-consumer.png";
+    $style = "<style>";
+}else{
+    $company = "Legal Injury Advocate";
+    $logo = "img/lin-logo.png";
+}
+?>
 <!doctype html>
 <html>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Legal Injury Network</title>
+    <title>{{$company}}</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <!-- <link href="/layouts/3/css/style.css" rel="stylesheet"> -->
-    @php
-    $phone = "(800) 484-6757";
-    $tel = "8004846757";
-    $year = date("Y");
-    $url = url('');
-    $logo = "";
-    $company = "";
-    $style = "<style></style>";
-    @endphp
-    <?php
-    if(str_contains($url, 'consumer')){
-      $company = "Consumer Injury Advocate";
-      $logo = "img/lin-logo-consumer.png";
-      $style = "<style>";
-    }else{
-      $company = "Legal Injury Advocate";
-      $logo = "img/lin-logo.png";
-    }
-    ?>
+    
     <link href="css/2.style.css" rel="stylesheet">
     @if(isset($useXverify) && $useXverify == true)
     <link rel="stylesheet" type="text/css" href="https://www.xverify.com/css/ui_tooltip_style.css" />
